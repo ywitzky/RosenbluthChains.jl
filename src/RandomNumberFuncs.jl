f532de46d1457da91a7b72ee2bbc77d65b494905
Yannick Witzky <ywitzky@students.uni-mainz.de>
2023-07-05 09:50:13 +0200

    minor fixes, forgot in previous commit

--- src/RandomNumberFuncs.jl
+++ src/RandomNumberFuncs.jl
@@ -1,5 +1,8 @@
+using Random
+
+export _rand, _rand2π, _rand_off
 ### Random numbers
 _rand(len::T)     where {T<:Real} = rand(T) * len
 _rand_off(len::T) where {T<:Real} = rand(T) * len - len / 2.0
-_rand_off(A::T, len::T) where {T<:Real} = () -> rand(T) * len - len / 2.0
+_rand_off(len::T,A::T) where {T<:Real} = rand(T) * len + A
 _rand2π(len::T)   where {T<:Real} = _rand(2.0*T(π))

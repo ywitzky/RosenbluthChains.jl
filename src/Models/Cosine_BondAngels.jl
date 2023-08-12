47c98077356cd91b7f9ddc16ecd42af5ef66e3b8
Yannick Witzky <ywitzky@students.uni-mainz.de>
2023-08-12 18:58:20 +0200

    working implementation of Cosing Bond Angle tests.

--- src/Models/Cosine_BondAngels.jl
+++ src/Models/Cosine_BondAngels.jl
@@ -1,134 +1,194 @@
-export Cosine_BondAngles, SetTrialBondAngle, GaussianLp_Cosine_BondAngles, GaussianInvLp_Cosine_BondAngles
+export Cosine_BondAngles, SetTrialBondAngle, GaussianLp_Cosine_BondAngles, GaussianInvLp_Cosine_BondAngles, GaussianK_Cosine_BondAngles
 
-using  Distributions
+using  Distributions, Interpolations
+
+
+
+function integrate_Cosine_BondAngle_toA(K::T, A::T) where {T<:Real}
+    return (1.0-exp(K*(cos(A)-1.0)))/K
+end
+
+function Cosine_BondAngle_func(K::T, x::T) where {T<:Real}
+    return exp(-K*(1.0-cos(x)))*sin(x)
+end
+
+function maxima_pos(K::T) where {T<:Real}
+    return 2.0*atan(sqrt(sqrt(4.0*K^2+1.0)-2.0*K))
+end
+
+
+
+struct Cosine_BondAngle_Sampler{I<:Int, T<: Real} <: Sampleable{Univariate,Continuous}
+    K::T
+    N::I
+    borders::Vector{T}
+    width::T
+    likelyhood::Vector{T}
+    maxima::Vector{T}
+    work_array::Array{Bool}
+    Cosine_BondAngle_Sampler(K::T, N=30::Int64) where {T<:Real} = begin
+        borders = LinRange(0,π, N+1)
+
+        max_vals = [max( Cosine_BondAngle_func(K, borders[i]), Cosine_BondAngle_func(K, borders[i+1])) for i in 1:N]
+        ind =  findfirst(maxima_pos(K).>borders )
+        max_vals[ind] = max(max_vals[ind] , Cosine_BondAngle_func(K,maxima_pos(K)))
+        new{Int64, T}( K, N, borders, π/Float64(N), integrate_Cosine_BondAngle_toA.(K,borders[2:end]), max_vals, zeros(Bool, N))
+    end
+end
+
+function give_rand( s::Cosine_BondAngle_Sampler) 
+    #s.work_array.=(rand(eltype(s.likelyhood))*s.likelyhood[end]).<= s.likelyhood
+    bla = rand(eltype(s.likelyhood))*s.likelyhood[end]
+    ind = findfirst(x->x>=bla, s.likelyhood)
+    return rand_help(ind, s)
+end
+
+function rand_help(ind::Int64,s::Cosine_BondAngle_Sampler)
+    rand_pos = _rand_off(s.width, s.borders[ind])
+    while rand() > Cosine_BondAngle_func(s.K, rand_pos)/s.maxima[ind]
+        rand_pos = _rand_off(s.width, s.borders[ind])
+    end
+    return rand_pos
+end
+
+
 
 struct Cosine_BondAngles{T<:Real} <: AbstractBondAngleParam
     PersistenzLengths::Vector{T} 
-    K::Vector{T} 
-    Cosine_BondAngles(PersistenzLengths::Vector{T}) where {T<:Real} = new{T}(PersistenzLengths, zeros(T, length(PersistenzLengths)))
+    AngleGenerator::Vector{Cosine_BondAngle_Sampler{Int64, T}}
+    Cosine_BondAngles(PersistenzLengths::Vector{T}) where {T<:Real} = new{T}(PersistenzLengths, Cosine_BondAngle_Sampler.(solveRecursive.(lpToCosAngle.(PersistenzLengths),AvgCos , 0.1,200.0 ), 30))
 end
 
 ### is not optimised for very stiff polymers, where some values will never occur
-@inline function SetTrialBondAngle(data::SimData,Cosine_BondAngles)
-    #data.trial_angle.= rand(eltype(data.TType), data.NTrials) .* eltype(data.TType)(π)
-    rand!(data.trial_angle, eltype(data.TType))
-    data.trial_angle.*= eltype(data.TType)(π)
+@inline function SetTrialBondAngle(data::SimData,param::Cosine_BondAngles)
+    for i in 1:data.NTrials
+        data.trial_angle[i] =  give_rand(param.AngleGenerator[data.id])
+    end
     CompTrigonometricTrialBondAngles(data)
 end
 
 function GetTrialBoltzmannWeight(data::SimData,param::Cosine_BondAngles)
-    data.LogBoltzmannFaktor .-= @. param.K[data.id]*(1.0-data.cos_trial_angle)
     nothing
 end
 
 ### Init function doesnt lp values larger ≈700A and smaller than 2A
 @inline function InitSimParam(data::SimData,param::Cosine_BondAngles ) 
-    #angles=collect(range(0,π/2.0,10_000))
-    # K->AvgCos(K, angles)
-    param.K .= solveRecursive.(lpToCosAngle.(param.PersistenzLengths),AvgCos , 0.1,200.0 )
-    #println("K $(param.K)")
+    nothing
 end
 
 
 
-
-
-
-### Analytical solution to <cos(x)>=integral cos(x)exp(+K(1+cos(x)))/Z; assuming K is negativ to have potential
-#function AvgCos(K)
-#    return besseli1(K)/besseli0(K)
-#end
-#=
-function AvgCos(K, x)
-    f(y)=@. cos(y)^2*exp(-1.0*K*(1.0-cos(y)))
-    g(y)=@. cos(y)*exp(-1.0*K*(1.0-cos(y)))
-    return integrate(x, f(x))/integrate(x, g(x))
-end
-=#
 function AvgCos(K) #### coth(K)-1/K
-    return  1/K*(exp(-2.0*K)*(exp(2.0*K)*(K-1.0)+K+1.0))/(1.0-exp(-2.0*K))
+    return  (coth(K)-1.0/K) #1.0/K*(exp(-2.0*K)*(exp(2.0*K)*(K-1.0)+K+1.0))/(1.0-exp(-2.0*K))
 end
 
-### Same potential but, K's are drawn such that lp is Gaussian distributed
-mutable struct GaussianLp_Cosine_BondAngles{T<:Real, I<:Integer} <: AbstractBondAngleParam
-    PersistenceDist::Normal{T}
-    N_Hashes::Int64
-    K_Hashfunc::Function
-    K_HashTable::Vector{T}
-    K::Vector{T} 
+
+
+
+struct Cosine_BondAngle_Prefactor_Sampler{I<:Int, T<: Real} <: Sampleable{Univariate,Continuous}
     μ::T
     σ::T
-    GaussianLp_Cosine_BondAngles(μ::T, σ::T; N_Hashes::I=20000, K_Hashfunc=x-> trunc(Int32,(x-(μ-5.0*σ))*(N_Hashes)/(10.000000001*σ)+1)) where {T<:Real, I<:Integer} = new{T,I}(Normal(μ, σ), N_Hashes,K_Hashfunc, zeros(T,N_Hashes), zeros(T, 1), μ, σ)
+    K_borders::Vector{T}
+    K_cdf::Vector{T}
+    K_max::Vector{}
+    K_width::T
+    max_val::T
+    P::Any
+    Cosine_BondAngle_Prefactor_Sampler(μ::T, σ::T, P, K; NK=10::Int64, NA=10::Int64) where {T<:Real} = begin
+        K_borders = collect(LinRange(μ-5.0*σ,μ+5.0*σ, NK+1))
+        K_width= K_borders[2]-K_borders[1]
+
+        K_mean = (K_borders[2:end].+K_borders[1:end-1])/2.0
+        K_cdf = cdf.(Normal(μ, σ), K_borders[2:end])
+        K_max  = [max(pdf(Normal(μ, σ),K_borders[i]), pdf(Normal(μ, σ),K_borders[i+1]) ) for i in 1:NK]
+        K_ind =  findfirst(μ.<=K_borders) -1
+        K_max[K_ind] = max(pdf(Normal(μ, σ),μ), K_max[K_ind])
+        max_val = maximum([P(k,maxima_pos(K(k))) for k in  collect(LinRange(μ-1.0*σ,μ+1.0*σ, 100))]) ### safety margin
+        new{Int64, T}(μ, σ,K_borders,K_cdf,K_max, K_width,max_val, P)
+    end
+end
+
+
+function give_rand( s::Cosine_BondAngle_Prefactor_Sampler)
+    ### draw gaussian for K or lp
+    rand_K  = _rand_off(10.0*s.σ, s.μ-5.0*s.σ)
+    rnd_num = rand(eltype(s.K_cdf))*s.K_cdf[end]
+    ind = findfirst(x->x>=rnd_num, s.K_cdf)
+    rand_K = _rand_off(s.K_width, s.K_borders[ind])
+    while rand()*s.K_max[ind] > pdf(Normal(s.μ, s.σ), rand_K)
+        rand_K = _rand_off(s.K_width,  s.K_borders[ind])
+    end
+
+    ### draw angle accordingly
+    rand_ϕ = rand(eltype(rand_K))*π
+    while rand()*s.max_val >  s.P(rand_K,rand_ϕ)
+        rand_ϕ = rand(eltype(rand_K))*π
+    end
+    return (rand_K, rand_ϕ)
+end
+
+
+
+### Same potential but, K's are drawn such that lp is Gaussian distributed
+mutable struct GaussianK_Cosine_BondAngles{T<:Real} <: AbstractBondAngleParam
+    μ::T
+    σ::T
+    Sampler::Cosine_BondAngle_Prefactor_Sampler
+    GaussianK_Cosine_BondAngles(μ::T, σ::T) where {T<:Real} = begin 
+        new{T}(μ, σ, Cosine_BondAngle_Prefactor_Sampler(μ, σ, Cosine_BondAngle_func, x->x))
+    end
+end
+
+### Init function doesnt allow <cos> values larger 1000 and smaller than ≈1/3
+function InitSimParam(data::SimData,param::GaussianK_Cosine_BondAngles ) 
+    nothing
+end
+
+### is not optimised for very stiff polymers, where some values will never occur
+@inline function SetTrialBondAngle(data::SimData,param::GaussianK_Cosine_BondAngles)
+    for i in 1:data.NTrials
+        data.trial_angle[i] = give_rand(param.Sampler)[2]
+    end
+    CompTrigonometricTrialBondAngles(data)
+    nothing
+end
+
+@inline function GetTrialBoltzmannWeight(data::SimData,param::GaussianK_Cosine_BondAngles)
+    nothing
+end
+
+
+##############################
+
+### Same potential but, K's are drawn such that lp is Gaussian distributed
+mutable struct GaussianLp_Cosine_BondAngles{T<:Real} <: AbstractBondAngleParam
+    μ::T
+    σ::T
+    Sampler::Cosine_BondAngle_Prefactor_Sampler
+    GaussianLp_Cosine_BondAngles(μ::T, σ::T) where {T<:Real} = begin
+        ### make linear interpolation
+        lp = collect(LinRange(μ-6.0*σ, μ+6.0*σ, 500))
+        K_val= solveRecursive.(lpToCosAngle.(lp),AvgCos , 0.0001,10000.0 )
+        K = linear_interpolation(lp, K_val)
+        H(x1,x2) = Cosine_BondAngle_func(K(x1), x2)
+        new{T}(μ, σ, Cosine_BondAngle_Prefactor_Sampler(μ, σ, H, K))
+    end
 end
 
 ### Init function doesnt allow <cos> values larger 1000 and smaller than ≈1/3
 function InitSimParam(data::SimData,param::GaussianLp_Cosine_BondAngles ) 
-    ### Init K_HashTable
-    for lp in range(param.μ-5.0*param.σ,param.μ+5.0*param.σ, param.N_Hashes)
-        param.K_HashTable[param.K_Hashfunc(lp)] = solveRecursive.(lpToCosAngle.(lp),AvgCos , 0.1,200.0 )
-    end
-    param.K=zeros(eltype(data.TType), data.NTrials)
+    nothing
 end
 
 ### is not optimised for very stiff polymers, where some values will never occur
 @inline function SetTrialBondAngle(data::SimData,param::GaussianLp_Cosine_BondAngles)
-    data.trial_angle.= rand(eltype(data.TType), data.NTrials) .* eltype(data.TType)(π)
+    for i in 1:data.NTrials
+        data.trial_angle[i] = give_rand(param.Sampler)[2]
+    end
+    CompTrigonometricTrialBondAngles(data)
+    nothing
 end
 
 function GetTrialBoltzmannWeight(data::SimData,param::GaussianLp_Cosine_BondAngles)
-    ### get hashed K values according to Gaus distribution of lp   
-    data.tmp4 .= rand(param.PersistenceDist, data.NTrials)
-    for (id,val) in  enumerate(data.tmp4)
-         param.K[id] = get(param.K_HashTable, param.K_Hashfunc(val) , solveRecursive.(lpToCosAngle(val), AvgCos, 0.1,200.0 ))
-    end
-    data.LogBoltzmannFaktor .-= @. param.K[1]*(1-cos(data.trial_angle))
     nothing
 end
-
-
-
-
-
-
-
-
-## Same potential but, K's are drawn such that lp is Gaussian distributed
-mutable struct GaussianInvLp_Cosine_BondAngles{T<:Real, I<:Integer} <: AbstractBondAngleParam
-    InvPersistenceDist::Normal{T}
-    N_Hashes::Int64
-    K_Hashfunc::Function
-    K_HashTable::Vector{T}
-    K::Vector{T} 
-    μ::T
-    σ::T
-    GaussianInvLp_Cosine_BondAngles(μ::T, σ::T; N_Hashes::I=20000, K_Hashfunc=x-> trunc(Int32,(x-(μ-5.0*σ))*(N_Hashes)/(10.000000001*σ)+1)) where {T<:Real, I<:Integer} = new{T,I}(Normal(μ, σ), N_Hashes,K_Hashfunc, zeros(T,N_Hashes), zeros(T, 1), μ, σ)
-end
-
-
-### Init function doesnt allow <cos> values larger 1000 and smaller than ≈1/3
-function InitSimParam(data::SimData,param::GaussianInvLp_Cosine_BondAngles ) 
-    ### Init K_HashTable
-    for InvLp in range(param.μ-5.0*param.σ,param.μ+5.0*param.σ, param.N_Hashes)
-        param.K_HashTable[param.K_Hashfunc(InvLp)] = solveRecursive.(InvlpToCosAngle.(InvLp),AvgCos , 0.1,200.0 )
-    end
-    param.K=zeros(eltype(data.TType), data.NTrials)
-end
-
-
-### is not optimised for very stiff polymers, where some values will never occur
-@inline function SetTrialBondAngle(data::SimData,param::GaussianInvLp_Cosine_BondAngles)
-    data.trial_angle.= rand(eltype(data.TType), data.NTrials) * eltype(data.TType)(π)
-end
-
-
-function GetTrialBoltzmannWeight(data::SimData,param::GaussianInvLp_Cosine_BondAngles)
-    ### get hashed K values according to Gaus distribution of lp   
-    data.tmp4 .= rand(param.InvPersistenceDist, 1)#data.NTrials)
-    for (id,val) in  enumerate(data.tmp4[1:1])
-         param.K[id] =  solveRecursive.(InvlpToCosAngle(BigFloat(val)), AvgCos, BigFloat(0.1),BigFloat(200.0) )
-         #get(param.K_HashTable, param.K_Hashfunc(val) , solveRecursive.(InvlpToCosAngle(val), AvgCos, 0.1,200.0 ))
-    end
-    data.LogBoltzmannFaktor .-= @. param.K[1]*(1-cos(data.trial_angle))
-    nothing
-end
-
